e6bfcb79798266c9628e18cfb8e0d8842b0e2ef5
TimberTrader <tp.bowes@gmail.com>
2019-06-14 18:07:05 -0600

    coded generators only to find they are being deprecretad in v3 / switching to new tutorial ... ooops

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const koa = require('koa');
 const app = new koa();
 
-app.use(function* (){
-  this.body= 'Hello World';
+app.use(function* (next){
+  // do something before yielding to next generator function
+
+  // in line - 1st event downstream
+  console.log('1');
+  yield next;
+
+  //do anotherthiung when execution returns upstream
+  //last event upstream
+  console.log('2');
 });
 
+app.use(function* (next){
+  // 2nd event downstream
+  console.log('3');
+  yield next;
+
+  //2nd event upstream
+  console.log('4');
+
+app.use(function* (next){
+  // 3rd & last event downstream
+  console.log('5');
+
+  // set response body
+  this.body= 'Hello Generators';
+
+  //1st event upstream
+  console.log('6');
+
+})
+})
 app.listen(3000, function(){
   console.log('Sever running on https://localhost:3000')
 });
